7d84097d7ea82faf85f8a5ee2f9dabc91d1d5b09
juancueto <juan.cueto@outlook.com>
2022-09-30 12:42:40 -0500

    ejercicio 8 - Clase Persona

---
+++ Open-BootCamp/Ejercicio8.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Open_BootCamp
+{
+    public class Persona
+    {
+        private int _edad;
+        private string _nombre;
+        private string _telefono;
+
+        public int Edad
+        {
+            get { return this._edad; }
+            set { this._edad = value; }
+        }
+
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+
+        public string Telefono
+        {
+            get { return this._telefono; }
+            set { this._telefono = value; }
+        }
+    }
+}
+
---
+++ Open-BootCamp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Open_BootCamp;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Persona obj = new Persona();
+obj.Edad = 37;
+obj.Nombre = "Juan";
+obj.Telefono = "+51 567 786 899";
+
+Console.WriteLine(obj.Nombre);
+Console.WriteLine(obj.Edad);
+Console.WriteLine(obj.Telefono);
+
